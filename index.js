98631e006c4e838e4611aaa2b3124ca9a104f783
sixone <931039665@qq.com>
2018-11-13 12:32:04 +0800

    模板更新

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+	// 模版标识
+	name: 'example',
+	// 模版引擎
+	engine: 'pug',
+
+	// 页面
+	pages: [
+		{
+			name: '首页',
+			// 路径
+			path: '/',
+			// 预览图
+			thumbnail: 'home.png',
+		}
+	]
+}
